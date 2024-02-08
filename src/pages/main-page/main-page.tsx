4cf317d96289569806ba660a7ae3ae71ac8ad87c
DmitriiTMS <dmitrii12_88@mail.ru>
2024-02-08 16:55:33 +0300

    feat: add icons

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { List, Typography } from 'antd';
-const { Paragraph} = Typography;
+const { Paragraph } = Typography;
+
+import heart from '../../assets/icons/heart.svg';
+import calendar from '../../assets/icons/calendar.svg';
+import profile from '../../assets/icons/profile.svg';
 
 import styles from './main-page.module.css';
 
@@ -24,7 +28,36 @@ export const MainPage: React.FC = () => {
                 dataSource={data}
                 renderItem={(item) => <List.Item>{item}</List.Item>}
             />
-            <Paragraph className={styles.mainText}>CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!</Paragraph>
+            <Paragraph className={styles.mainText}>CleverFit — это
+                не просто приложение, а твой личный помощник в мире фитнеса.
+                Не откладывай на завтра — начни
+                тренироваться уже сегодня!
+            </Paragraph>
+
+            <div className={styles.list}>
+                <div className={styles.list__item}>
+                    <Paragraph className={styles.list__item_title}>Расписать тренировки</Paragraph>
+                    <Paragraph className={styles.content}>
+                        <img className={styles.content__img} src={heart} alt="heart" />
+                        <span className={styles.content__span}>Тренировки</span>
+                    </Paragraph>
+                </div>
+                <div className={styles.list__item}>
+                    <Paragraph className={styles.list__item_title}>Назначить календарь</Paragraph>
+                    <Paragraph className={styles.content}>
+                        <img className={styles.content__img} src={calendar} alt="calendar" />
+                        <span className={styles.content__span}>Календарь</span>
+                    </Paragraph>
+                </div>
+                <div className={styles.list__item}>
+                    <Paragraph className={styles.list__item_title}>Расписать тренировки</Paragraph>
+                    <Paragraph className={styles.content}>
+                        <img className={styles.content__img} src={profile} alt="profile" />
+                        <span className={styles.content__span}>Профиль</span>
+                    </Paragraph>
+                </div>
+            </div>
+
         </div>
     )
 };
